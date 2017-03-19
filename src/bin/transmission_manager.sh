9bb5c95e0de284b01fd1283bae0836020ce8554d
riccardo <riccardo@192.168.1.43>
2017-03-19 23:36:04 +0100

    filtering the sh script

--- src/bin/transmission_manager.sh
+++ src/bin/transmission_manager.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/java -classpath /usr/local/share/java/ServerPack-0.0.27-jar-with-dependencies.jar it.fraguglia.transmission.TransmissionManager $@
+/usr/bin/java -classpath /usr/local/share/java/${project.build.finalName}-jar-with-dependencies.jar it.fraguglia.transmission.TransmissionManager $@
